096bb068ee9b7287352a258b9893ee2bb06f962a
Suzin <patrycjusz.sosnowski@gmail.com>
2019-12-13 09:44:14 +0100

    Revert "add DI to main controller."
    This reverts commit 87eb2720da1504f26bcd9d223ea5a829213ded6d.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,28 +6,32 @@ use App\Model\JobOfferContent;
 use App\Model\JobOfferWriter;
 use App\Entity\Job;
 use App\Entity\Skills;
+
+//use Symfony\Component\Debug\Debug;
+use \Doctrine\Common\Util\Debug;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
 
 class MainController extends AbstractController
 {
-    /**
-     * @var JobOfferWriter
-     */
-    private $jobOfferWriter;
-
-    public function __construct(JobOfferWriter $jobOfferWriter)
-    {
-        $this->jobOfferWriter = $jobOfferWriter;
-    }
-
     /**
      * @Route("", name = "index")
      */
     public function index()
     {
+        
+        // $url = 'https://justjoin.it/api/offers';
+        // $data = [];
+        // $jobOffers = new JobOfferContent();
+        // $offers = $jobOffers->getJobOffers($url);
+        // $this->saveOffers($offers);
         return $this->render('main_body.html.twig');
     }
 
@@ -41,27 +45,29 @@ class MainController extends AbstractController
         $jobOffers = new JobOfferContent();
         $offers = $jobOffers->getJobOffers($url);
         $this->saveOffers($offers);
-
         return $this->redirectToRoute('index');        
     }
 
     public function saveOffers(array $offers): void
     {
         $count = 0;
-        //$this->jobOfferWriter->setDoctrine($this->getDoctrine());
+        $jobOfferWriter = new JobOfferWriter();
+        //passing doctrine to jobofferwritter bc for some reason getDoctrine() method not working in JobofferWriter file , 
+        //dont know why.
+        $jobOfferWriter->setDoctrine($this->getDoctrine());
         $lastUpdate = $this->lastByDate();
         foreach($offers as $job){
             if($lastUpdate < \strtotime($job['published_at'])){
                 $count++;
-                $this->jobOfferWriter->settingJob($job);
+                $jobOfferWriter->settingJob($job);
             }          
         }
+        //dump($count);
     }
 
     public function lastByDate():int
     {
         $lastentry = $this->getDoctrine()->getRepository(Job::class)->findOneBy([],['published_at'=>'DESC']);
-
         return \strtotime($lastentry->getPublishedAt());    
     }
 
@@ -74,4 +80,25 @@ class MainController extends AbstractController
         $rows = $stmt->fetchAll();
         return $rows[0]['NUM_ROWS'];
     }
+    
+    // /**
+    //  * @Route("/uuuuuuupdate", name = "iuuuuuuupdate")
+    //  */
+    // public function update()
+    // {
+
+    //     $url = 'https://justjoin.it/api/offers';
+    //     $data = [];
+    //     $jobOffers = new JobOfferContent();
+    //     $offers = $jobOffers->getJobOffers($url);
+        
+    //     $offers_from_db = $this->getDoctrine()->getRepository(Job::class)->findBy([],['id'=>'DESC'],10);
+        
+    //     dump($offers_from_db, $this);
+    //     $entityManager = $this->getDoctrine()->getManager();
+
+    //     return $this->render('base.html.twig');
+    // }
+
+    
 }
