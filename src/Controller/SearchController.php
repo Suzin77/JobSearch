97bdea1928b9d151d026903dcde34eaed05ae5c0
Suzin <patrycjusz.sosnowski@gmail.com>
2020-01-06 22:50:35 +0100

    add pagination to search controller.

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Job;
 use App\Form\SearchJobType;
 use App\Repository\JobRepository;
+use App\Service\SearchPaginator;
 use Doctrine\Common\Collections;
 use Doctrine\Common\Collections\Criteria as Criteria;
 use Doctrine\Common\Persistence\ObjectRepository;
@@ -13,6 +14,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionBagInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -29,46 +32,71 @@ class SearchController extends AbstractController
      * @var Criteria
      */
     private $criteria;
+    /**
+     * @var SearchPaginator
+     */
+    private $searchPaginator;
+    /**
+     * @var JobRepository
+     */
+    private $jobRepository;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, SearchPaginator $searchPaginator, JobRepository $jobRepository)
     {
         $this->em = $entityManager;
+        $this->searchPaginator = $searchPaginator;
+        $this->jobRepository = $jobRepository;
         $this->criteria = new \Doctrine\Common\Collections\Criteria();
     }
 
     /**
-     * @Route("/search", name = "search")
+     * @Route("/search/{page}", name = "search")
      * @param Request $request
+     * @param Session $session
      * @return Response
      * @throws \Doctrine\ORM\Query\QueryException
      */
-    public function main(Request $request): Response
+    public function main(Request $request, Session $session): Response
     {
         $form = $this->createForm(SearchJobType::class);
         $form->handleRequest($request);
+        $page = $request->get('page');
+        $data = NULL;
 
-        $jobs = $this->em->getRepository(Job::class);
-        
-        if($form->isSubmitted() && $form->isValid()){
+        if($page>=1){
+            $data = $session->get('searchData');
+            $queryBuilder = $this->prepareQuery($data);
 
+            $this->searchPaginator->init($queryBuilder);
+            $resultCount = $this->searchPaginator->numberOfEntries;
+
+            $firstData=$this->searchPaginator->getData($page);
+            $data['page']= $page;
+            $session->set('searchData',$data);
+            $data['dbData']=$firstData;
+            $data['dbData_count'] = $resultCount;
+        }
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $page= 1;
             $data = $form->getData();
 
-            $queryBuilder = $jobs->createQueryBuilder('job');
-            $this->addMyCriteria('city',$data['city']);
-            $this->addMyCriteria('experience_level', $data['experience_level']);
-            $this->addMyCriteria('marker_icon',$data['tech']);
+            $queryBuilder = $this->prepareQuery($data);
 
-            $queryBuilder->addCriteria($this->criteria);
-            $queryBuilder->addOrderBy('job.published_at','DESC');
+            $this->searchPaginator->init($queryBuilder);
+            $resultCount = $this->searchPaginator->numberOfEntries;
 
-            $firstData=$queryBuilder->getQuery()->execute();
+            $firstData=$this->searchPaginator->getData($page);
 
+            $data['page']= $page;
+            $session->set('searchData',$data);
             $data['dbData']=$firstData;
-            $data['dbData_count'] = count($firstData);
+            $data['dbData_count'] = $resultCount;
 
-            return $this->render('search.html.twig', ['my_form'=>$form->createView(), 'data'=>$data]);
+            //return $this->render('search.html.twig',['my_form'=>$form->createView(),'data'=>$data, 'page'=>$page]);
         }
-        return $this->render('search.html.twig',['my_form'=>$form->createView()]);
+
+        return $this->render('search.html.twig',['my_form'=>$form->createView(),'data'=>$data,'page'=>$page]);
     }
 
     public function addMyCriteria($field, $value)
@@ -78,4 +106,16 @@ class SearchController extends AbstractController
         }
     }
 
+    public function prepareQuery($data)
+    {
+        $queryBuilder = $this->jobRepository->createQueryBuilder('job');
+        $this->addMyCriteria('city',$data['city']);
+        $this->addMyCriteria('experience_level', $data['experience_level']);
+        $this->addMyCriteria('marker_icon',$data['tech']);
+
+        $queryBuilder->addCriteria($this->criteria);
+        $queryBuilder->addOrderBy('job.published_at','DESC');
+
+        return $queryBuilder;
+    }
 }
